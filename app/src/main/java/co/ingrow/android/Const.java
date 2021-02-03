43e19dc5d2618dfec6c5bb5b7e9febc1f5dc526b
Meikiem <safari.langaroudi@gmail.com>
2021-02-03 16:29:45 +0330

    Update: IP Enrichment added

--- app/src/main/java/co/ingrow/android/Const.java
+++ app/src/main/java/co/ingrow/android/Const.java
@@ -8,6 +8,8 @@ public class Const {
     public static final String ENRICHMENT = "enrichment";
     public static final String NAME = "name";
     public static final String SESSION = "session";
+    public static final String IP = "ip";
+    public static final String AUTO_FILL = "autofill";
     public static final String INPUT = "input";
     public static final String API_KEY = "api-key";
 }
